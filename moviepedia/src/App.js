36579f33ac43fbb21a003c37cff6dafe7818f3f5
Gamddalki <hannahk01@naver.com>
2023-01-29 01:35:44 +0900

    Update App.js
    useEffect : 데이터 값의 변경이 있을 경우에만 실행되게끔 해줌

--- moviepedia/src/App.js
+++ moviepedia/src/App.js
@@ -1,10 +1,29 @@
-import Button from "./Button";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("YEAH");
+  useEffect(() => {
+    console.log("CALL API...");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "") {
+      console.log("SEARCHING", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
-      <h1>Hi!</h1>
-      <Button text="Join" />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Anything u wanna search?"
+      ></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me!</button>
     </div>
   );
 }
