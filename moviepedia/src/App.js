3278a749c46f76c219f9e5e06241da8996415323
Gamddalki <hannahk01@naver.com>
2023-01-31 02:07:07 +0900

    react-router-dom version upgrade
    & 리팩토링

--- moviepedia/src/App.js
+++ moviepedia/src/App.js
@@ -1,46 +1,5 @@
-import { useEffect, useState } from "react";
-import PropTypes from "prop-types";
-import Movie from "./Movie";
+import { BrowserRouter as Router, Swtich, Route } from "react-router-dom";
 
-function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-    <div>
-      <h1>{movies.length} Movies here!</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <div>
-        {movies.map((movie) => (
-          <Movie
-            key={movie.id}
-            title={movie.title}
-            poster={movie.medium_cover_image}
-            summary={movie.summary}
-            genres={movie.genres}
-          />
-        ))}
-      </div>
-    </div>
-  );
-}
-
-Movie.PropTypes = {
-  title: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired,
-  summary: PropTypes.string.isRequired,
-  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
-};
+function App() {}
 
 export default App;
